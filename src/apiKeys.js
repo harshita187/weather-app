0cffea83dde94c8e00b72be3a2c1e68c66f94826
harshita187 <harsita129singh@gmail.com>
2024-06-08 19:27:29 +0530

    code added for api data fetch

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5a8c86b6b4a799006c8bceb23fbf3304",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
